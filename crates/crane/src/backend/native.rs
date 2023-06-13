bb52d8c8bb056db6cd227bd01fc6890cd749bc48
Marshall Bowers <elliott.codes@gmail.com>
2023-06-13 10:46:13 -0400

    Emit returns in functions

--- crates/crane/src/backend/native.rs
+++ crates/crane/src/backend/native.rs
@@ -1,4 +1,5 @@
 use inkwell::context::Context;
+use inkwell::passes::PassManager;
 use inkwell::targets::{
     CodeModel, FileType, InitializationConfig, RelocMode, Target, TargetTriple,
 };
@@ -42,6 +43,13 @@ impl NativeBackend {
         let target_machine = dbg!(target_machine);
 
         let module = self.context.create_module("main");
+        let builder = self.context.create_builder();
+
+        let fpm = PassManager::create(&module);
+
+        fpm.add_instruction_combining_pass();
+
+        fpm.initialize();
 
         for item in program {
             match item.kind {
@@ -50,7 +58,23 @@ impl NativeBackend {
 
                     let fn_value = module.add_function(&item.name.to_string(), fn_type, None);
 
+                    let entry = self.context.append_basic_block(fn_value, "entry");
+
+                    builder.position_at_end(entry);
+
+                    builder.build_return(None);
+
                     dbg!(fn_value);
+
+                    if fn_value.verify(true) {
+                        fpm.run_on(&fn_value);
+
+                        println!("{} is verified!", item.name);
+                    } else {
+                        println!("{} is not verified :(", item.name);
+                    }
+
+                    fn_value.print_to_stderr();
                 }
             }
         }
