b4c7f4cc7d5ff98e74600ef8e78ae026f65cc4d9
Marshall Bowers <elliott.codes@gmail.com>
2023-06-17 20:58:40 -0400

    Parse `let` bindings

--- crates/crane/src/parser/stmt.rs
+++ crates/crane/src/parser/stmt.rs
@@ -1,13 +1,28 @@
-use crate::ast::{Stmt, StmtKind};
-use crate::lexer::token::Token;
+use tracing::trace;
+
+use crate::ast::{keywords, Local, LocalKind, Stmt, StmtKind};
+use crate::lexer::token::{Token, TokenKind};
 use crate::lexer::LexError;
-use crate::parser::{ParseResult, Parser};
+use crate::parser::{ParseError, ParseErrorKind, ParseResult, Parser};
 
 impl<TokenStream> Parser<TokenStream>
 where
     TokenStream: Iterator<Item = Result<Token, LexError>>,
 {
     pub fn parse_stmt(&mut self) -> ParseResult<Option<Stmt>> {
+        trace!("parse_stmt");
+
+        if self.consume_keyword(keywords::LET) {
+            let local = self.parse_local()?;
+
+            let span = local.span;
+
+            return Ok(Some(Stmt {
+                kind: StmtKind::Local(Box::new(local)),
+                span,
+            }));
+        }
+
         if let Some(expr) = self.parse_expr()? {
             let span = expr.span;
 
@@ -18,5 +33,30 @@ where
         }
 
         Ok(None)
+    }
+
+    fn parse_local(&mut self) -> ParseResult<Local> {
+        trace!("parse_local");
+
+        let name = self.parse_ident()?;
+
+        self.consume(TokenKind::Equal);
+
+        let init = self.parse_expr()?.ok_or_else(|| ParseError {
+            kind: ParseErrorKind::Error(format!(
+                "Expected an initializer for this `{}` binding.",
+                keywords::LET
+            )),
+            span: self.token.span,
+        })?;
+
+        let span = name.span;
+
+        Ok(Local {
+            kind: LocalKind::Init(Box::new(init)),
+            name,
+            span,
+            ty: None,
+        })
     }
 }
