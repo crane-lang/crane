d825d530381e110326c023c3d41df471e795588d
Marshall Bowers <elliott.codes@gmail.com>
2023-06-18 14:09:40 -0400

    Alphabetize keywords (#29)
    This PR alphabetizes the list of keywords.

--- crates/crane/src/ast/keywords.rs
+++ crates/crane/src/ast/keywords.rs
@@ -2,13 +2,18 @@ use smol_str::SmolStr;
 
 use crate::ast::{Ident, DUMMY_SPAN};
 
-pub const PUB: Ident = Ident {
-    name: SmolStr::new_inline("pub"),
+pub const FN: Ident = Ident {
+    name: SmolStr::new_inline("fn"),
     span: DUMMY_SPAN,
 };
 
-pub const FN: Ident = Ident {
-    name: SmolStr::new_inline("fn"),
+pub const LET: Ident = Ident {
+    name: SmolStr::new_inline("let"),
+    span: DUMMY_SPAN,
+};
+
+pub const PUB: Ident = Ident {
+    name: SmolStr::new_inline("pub"),
     span: DUMMY_SPAN,
 };
 
@@ -19,10 +24,5 @@ pub const STRUCT: Ident = Ident {
 
 pub const UNION: Ident = Ident {
     name: SmolStr::new_inline("union"),
-    span: DUMMY_SPAN,
-};
-
-pub const LET: Ident = Ident {
-    name: SmolStr::new_inline("let"),
     span: DUMMY_SPAN,
 };
