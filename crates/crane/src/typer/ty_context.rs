bb76d7cfb8ba5c5b0fab5462b03097ec8c2abd45
Marshall Bowers <elliott.codes@gmail.com>
2023-06-27 23:38:26 -0400

    Add `Fn` variant to `TyKind` (#56)
    This PR adds an `Fn` variant to the `TyKind` enum.

--- crates/crane/src/typer/ty_context.rs
+++ crates/crane/src/typer/ty_context.rs
@@ -2,16 +2,25 @@ use typed_arena::Arena as TypedArena;
 
 use crate::interned::Interned;
 use crate::typer::ty::{Ty, TyKind};
-use crate::typer::UintTy;
+use crate::typer::{FnSig, UintTy};
 
 #[derive(Default)]
 pub struct Arena<'ctx> {
     pub tys: TypedArena<TyKind<'ctx>>,
+    pub ty_lists: TypedArena<Vec<Ty<'ctx>>>,
 }
 
 impl<'ctx> Arena<'ctx> {
     pub fn intern_ty(&self, kind: TyKind<'ctx>) -> Ty {
         Ty(Interned::new_unchecked(self.tys.alloc(kind)))
+    }
+
+    pub fn intern_fn(&'ctx self, params: Vec<Ty<'ctx>>, return_ty: Ty<'ctx>) -> Ty<'ctx> {
+        let params = self.ty_lists.alloc(params);
+
+        Ty(Interned::new_unchecked(
+            self.tys.alloc(TyKind::Fn(FnSig { params, return_ty })),
+        ))
     }
 }
 
